9deaa2b82969b69ff6cd4319c72c331cb22ed8da
esrrhs <esrrhs@163.com>
2020-04-05 15:00:31 +0800

    add

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "md5.h"
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc, const char *argv[]) {
     FILE *ptr_myfile = fopen(argv[0], "rb");
@@ -8,21 +9,21 @@ int main(int argc, const char *argv[]) {
         return 1;
     }
 
-    char *buffer = new char[1024 * 1024];
+    char *buffer = (char *) malloc(1024 * 1024);
     int n = fread(buffer, 1, 1024 * 1024, ptr_myfile);
 
     unsigned char buf[16] = {0};
 
     MD5_CTX ctx;
     md5_init(&ctx);
-    md5_update(&ctx, reinterpret_cast<const BYTE *>(buffer), n);
-    md5_final(&ctx, reinterpret_cast<BYTE *>(buf));
+    md5_update(&ctx, (const BYTE *)(buffer), n);
+    md5_final(&ctx, (BYTE *)(buf));
 
     char out[32] = {0};
     for (int i = 0; i < 16; i++) {
         sprintf(out + (i * 2), "%02x", buf[i]);
     }
 
-    printf("%s\n", argv[0], out);
+    printf("%s\n", out);
     return 0;
 }
