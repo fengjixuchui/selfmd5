ad5a6825403ea5dd679ce37ec035d9e930cc7168
esrrhs <esrrhs@163.com>
2020-04-08 18:09:39 +0800

    add

--- build-asm.sh
+++ build-asm.sh
@@ -1,3 +1,3 @@
-gcc -Os -fdata-sections -ffunction-sections -flto  md5.S main.s -o selfmd5 -Wl,--gc-sections -Wl,--strip-all -nostdlib -nostdinc
+gcc -Os -fdata-sections -ffunction-sections -flto  md5.s main.s -o selfmd5 -Wl,--gc-sections -Wl,--strip-all -nostdlib -nostdinc
 strip -s selfmd5 
 sstrip selfmd5
