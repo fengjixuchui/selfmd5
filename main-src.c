1731e91b0dc826db571fec24d20b49682ead01bb
esrrhs <esrrhs@163.com>
2020-04-09 10:35:20 +0800

    add

--- main-src.c
+++ main-src.c
@@ -1,38 +1,9 @@
-/* 
- * MD5 hash in C and x86 assembly
- * 
- * Copyright (c) 2017 Project Nayuki. (MIT License)
- * https://www.nayuki.io/page/fast-md5-hash-implementation-in-x86-assembly
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- * - The above copyright notice and this permission notice shall be included in
- *   all copies or substantial portions of the Software.
- * - The Software is provided "as is", without warranty of any kind, express or
- *   implied, including but not limited to the warranties of merchantability,
- *   fitness for a particular purpose and noninfringement. In no event shall the
- *   authors or copyright holders be liable for any claim, damages or other
- *   liability, whether in an action of contract, tort or otherwise, arising from,
- *   out of or in connection with the Software or the use or other dealings in the
- *   Software.
- */
-
-//#include <stdio.h>
-
-
-/* Function prototypes */
-
 #include <fcntl.h>
 #include <unistd.h>
 
 #define BLOCK_LEN 64  // In bytes
 #define STATE_LEN 4  // In words
 
-// Link this program with an external C or x86 compression function
 extern void md5_compress(unsigned int state[static STATE_LEN], const unsigned char block[static BLOCK_LEN]);
 
 #define LENGTH_SIZE 8  // In bytes
