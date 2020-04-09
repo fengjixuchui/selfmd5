b3f353fa9d136d3295e093ad75bc54bbf3f4c0e0
esrrhs <esrrhs@163.com>
2020-04-09 09:15:30 +0800

    add

---
+++ main-src.c
@@ -0,0 +1,90 @@
+/* 
+ * MD5 hash in C and x86 assembly
+ * 
+ * Copyright (c) 2017 Project Nayuki. (MIT License)
+ * https://www.nayuki.io/page/fast-md5-hash-implementation-in-x86-assembly
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
+
+//#include <stdio.h>
+
+
+/* Function prototypes */
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BLOCK_LEN 64  // In bytes
+#define STATE_LEN 4  // In words
+
+// Link this program with an external C or x86 compression function
+extern void md5_compress(unsigned int state[static STATE_LEN], const unsigned char block[static BLOCK_LEN]);
+
+#define LENGTH_SIZE 8  // In bytes
+
+int main(int argc, char* argv[]) {
+    int filedesc = open(argv[0], O_RDONLY, 400);
+
+    char message[20 * 1024];
+    int len = read(filedesc, message, 20 * 1024);
+
+    unsigned int hash[STATE_LEN];
+
+    hash[0] = (unsigned int) (0x67452301);
+    hash[1] = (unsigned int) (0xEFCDAB89);
+    hash[2] = (unsigned int) (0x98BADCFE);
+    hash[3] = (unsigned int) (0x10325476);
+
+    int off;
+    for (off = 0; len - off >= BLOCK_LEN; off += BLOCK_LEN)
+        md5_compress(hash, &message[off]);
+
+    unsigned char block[BLOCK_LEN] = {0};
+    int rem = len - off;
+    int i;
+    for (i = 0; i < rem; i++) {
+        block[i] = message[off + i];
+    }
+
+    block[rem] = 0x80;
+    rem++;
+    if (BLOCK_LEN - rem < LENGTH_SIZE) {
+        md5_compress(hash, block);
+        for (i = 0; i < sizeof(block); i++) {
+            block[i] = 0;
+        }
+    }
+
+    block[BLOCK_LEN - LENGTH_SIZE] = (unsigned char) ((len & 0x1FU) << 3);
+    len >>= 5;
+    for (i = 1; i < LENGTH_SIZE; i++, len >>= 8)
+        block[BLOCK_LEN - LENGTH_SIZE + i] = (unsigned char) (len & 0xFFU);
+    md5_compress(hash, block);
+
+    const char *hex = "0123456789abcdef";
+    unsigned char *buf = (unsigned char *) &hash;
+    for (i = 0; i < 16; i++) {
+        char tmp[3] = {0};
+        tmp[0] = hex[(buf[i] >> 4) & 0xF];
+        tmp[1] = hex[(buf[i]) & 0xF];
+        write(1, tmp, 2);
+    }
+
+    exit(0);
+}
+
---
+++ md5-src.c
@@ -0,0 +1,76 @@
+
+#include "md5.h"
+
+/****************************** MACROS ******************************/
+WORD ROTLEFT(WORD a, WORD b) { return ((a << b) | (a >> (32 - b))); }
+
+WORD F(WORD x, WORD y, WORD z) { return ((x & y) | (~x & z)); }
+
+WORD G(WORD x, WORD y, WORD z) { return ((x & z) | (y & ~z)); }
+
+WORD H(WORD x, WORD y, WORD z) { return (x ^ y ^ z); }
+
+WORD I(WORD x, WORD y, WORD z) { return (y ^ (x | ~z)); }
+
+void FF(WORD *a, WORD b, WORD c, WORD d, WORD m, WORD s, WORD t) {
+    *a += F(b, c, d) + m + t;
+    *a = b + ROTLEFT(*a, s);
+}
+
+void GG(WORD *a, WORD b, WORD c, WORD d, WORD m, WORD s, WORD t) {
+    *a += G(b, c, d) + m + t;
+    *a = b + ROTLEFT(*a, s);
+}
+
+void HH(WORD *a, WORD b, WORD c, WORD d, WORD m, WORD s, WORD t) {
+    *a += H(b, c, d) + m + t;
+    *a = b + ROTLEFT(*a, s);
+}
+
+void II(WORD *a, WORD b, WORD c, WORD d, WORD m, WORD s, WORD t) {
+    *a += I(b, c, d) + m + t;
+    *a = b + ROTLEFT(*a, s);
+}
+
+/*********************** FUNCTION DEFINITIONS ***********************/
+void md5_compress(WORD * input, WORD * m) {
+
+    WORD tmp[4];
+
+    tmp[0] = input[0];
+    tmp[1] = input[1];
+    tmp[2] = input[2];
+    tmp[3] = input[3];
+
+    typedef void (*FuckFunc)(WORD *a, WORD b, WORD c, WORD d, WORD m, WORD s, WORD t);
+    FuckFunc ff[] = {&FF, &GG, &HH, &II};
+
+    const char p1[] = {0, 3, 2, 1};
+    const char p2[] = {1, 0, 3, 2};
+    const char p3[] = {2, 1, 0, 3};
+    const char p4[] = {3, 2, 1, 0};
+    const char mm[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13,
+                       2, 7, 12, 5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2, 0, 7, 14, 5, 12, 3, 10, 1, 8, 15,
+                       6, 13, 4, 11, 2, 9};
+    const char ss[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14,
+                       20, 5, 9, 14, 20, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 6, 10, 15, 21, 6,
+                       10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
+    const WORD tt[] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+                       0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+                       0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+                       0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+                       0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+                       0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+                       0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+                       0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
+    for (int i = 0; i < 64; ++i) {
+        FuckFunc f = ff[i / 16];
+        f(&tmp[p1[i % 4]], tmp[p2[i % 4]], tmp[p3[i % 4]], tmp[p4[i % 4]], m[mm[i]], ss[i], tt[i]);
+    }
+
+    input[0] += tmp[0];
+    input[1] += tmp[1];
+    input[2] += tmp[2];
+    input[3] += tmp[3];
+}
+
