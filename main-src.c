f21279db2d5ca3255d0e2fcbc83b8d440a4a2c6c
esrrhs <esrrhs@163.com>
2020-04-14 19:23:39 +0800

    add

--- main-src.c
+++ main-src.c
@@ -7,13 +7,13 @@
 /****************************** MACROS ******************************/
 #define ROTLEFT(a, b)  ((a << b) | (a >> (32 - b)))
 
-#define F(x, y, z)  ((x & y) | (~x & z))
+#define FF(x, y, z)  ((x & y) | (~x & z))
 
-#define G(x, y, z)  ((x & z) | (y & ~z))
+#define GG(x, y, z)  ((x & z) | (y & ~z))
 
-#define H(x, y, z) (x ^ y ^ z)
+#define HH(x, y, z) (x ^ y ^ z)
 
-#define I(x, y, z)  (y ^ (x | ~z))
+#define II(x, y, z)  (y ^ (x | ~z))
 
 static long double fsin_my(long double a) {
     long double res;
@@ -32,52 +32,61 @@ int main(int argc, char *argv[]) {
     char data[1024];
     short len = read(open(argv[0], 0, 0), data, sizeof(data));
 
-    v4si hash = {(unsigned int) (0x67452301), (unsigned int) (0xEFCDAB89), (unsigned int) (0x98BADCFE),
-                 (unsigned int) (0x10325476)};
+    unsigned int hash[] = {(unsigned int) (0x67452301), (unsigned int) (0xEFCDAB89), (unsigned int) (0x98BADCFE),
+                           (unsigned int) (0x10325476)};
 
-    const short new_len = 632;
-    const short off = new_len - (new_len % 64);
-
+    const short new_len = 568;
     data[len] = 0x80;
     *(unsigned long long *) (data + new_len) = len << 3;
-    unsigned int *m = (unsigned int *) &data[off];
 
-    v4si tmp = hash;
+    for (short off = 0; off < new_len; off += BLOCK_LEN) {
+        unsigned int *m = (unsigned int *) &data[off];
 
-    for (char i = 0; i < 64; ++i) {
-        const char p1[] = {0, 3, 2, 1};
-        const char mmstart[] = {0, 1, 5, 0};
-        const char mmstep[] = {1, 5, 3, 7};
+        unsigned int A = hash[0];
+        unsigned int B = hash[1];
+        unsigned int C = hash[2];
+        unsigned int D = hash[3];
+
         const char ss[] = {7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21};
 
-        unsigned int inc;
-        unsigned int b = tmp[p1[(i + 3) % 4]];
-        unsigned int c = tmp[p1[(i + 2) % 4]];
-        unsigned int d = tmp[p1[(i + 1) % 4]];
-        switch (i / 16) {
-            case 0:
-                inc = F(b, c, d);
-                break;
-            case 1:
-                inc = G(b, c, d);
-                break;
-            case 2:
-                inc = H(b, c, d);
-                break;
-            case 3:
-                inc = I(b, c, d);
-                break;
+        for (char i = 0; i < 64; ++i) {
+
+            unsigned int F;
+            int g;
+            switch (i / 16) {
+                case 0:
+                    F = FF(B, C, D);
+                    g = i;
+                    break;
+                case 1:
+                    F = GG(B, C, D);
+                    g = (5 * i + 1) % 16;
+                    break;
+                case 2:
+                    F = HH(B, C, D);
+                    g = (3 * i + 5) % 16;
+                    break;
+                case 3:
+                    F = II(B, C, D);
+                    g = (7 * i) % 16;
+                    break;
+            }
+
+            unsigned int K = (unsigned int) ((unsigned long long) 4294967296 * fsin_my(i + 1));
+
+            F += A + K + m[g];
+
+            A = D;
+            D = C;
+            C = B;
+            B = B + ROTLEFT(F, ss[(i / 16) * 4 + (i % 4)]);
         }
 
-        unsigned int mm = m[(mmstart[i / 16] + (i % 16) * mmstep[i / 16]) % 16];
-        unsigned int s = ss[(i / 16) * 4 + (i % 4)];
-        unsigned int t = (unsigned int) ((unsigned long long) 4294967296 * fsin_my(i + 1));
-
-        tmp[p1[i % 4]] += inc + mm + t;
-        tmp[p1[i % 4]] = b + ROTLEFT(tmp[p1[i % 4]], s);
+        hash[0] += A;
+        hash[1] += B;
+        hash[2] += C;
+        hash[3] += D;
     }
-
-    hash += tmp;
 
     unsigned char *buf = (unsigned char *) &hash[0];
     for (unsigned char i = 0; i < 32; i++) {
--- main-src.s
+++ main-src.s
@@ -5,104 +5,92 @@
 _start:
     movq	8(%rsp), %rdi
 
-	#pushq	%rbp
+	#pushq	%r14
 	#movq	%rsi, %r8
 	#xorl	%edx, %edx
 	#xorl	%esi, %esi
-	#pushq	%rbx
+	#pushq	%r13
 	#xorl	%eax, %eax
-	subq	$11120, %rsp
+	#pushq	%r12
+	#pushq	%rbp
+	#pushq	%rbx
+	movabsq	$-1167088121787636991, %rbx
+	subq	$1072, %rsp
 	#movq	(%r8), %rdi
+	leaq	624(%rsp), %r12
 	mov	    $2, %al
     syscall #call	open
 
-	mov	    $1024, %dx
-	leaq	80(%rsp), %rsi
+	movl	$1024, %edx
+	leaq	48(%rsp), %rsi
 	movl	%eax, %edi
 	xor	    %al, %al
     syscall #call	read
 
-	vmovdqu	.LC0(%rip), %xmm0
-	xorl	%r8d, %r8d
-	movabsq	$1445102447882210311, %rsi
+	movq	%rbx, 16(%rsp)
+	movabsq	$1167088121787636990, %rbx
+	movabsq	$1445102447882210311, %r8
 	movswq	%ax, %rdx
-	mov	    $16, %r9b
-	movabsq	$1517442620720155396, %rdi
+	movq	%rbx, 24(%rsp)
+	movabsq	$1517442620720155396, %r9
+	leaq	48(%rsp), %rbx
 	movq	%rdx, %rax
-	movb	$-128, 80(%rsp,%rdx)
+	movb	$-128, 48(%rsp,%rdx)
 	sall	$3, %eax
-	vmovaps	%xmm0, 32(%rsp)
 	cltq
-	vmovaps	%xmm0, 48(%rsp)
-	movq	%rax, 712(%rsp)
+	movq	%rax, 616(%rsp)
+.L7:
+	movl	16(%rsp), %r13d
+	movl	24(%rsp), %edi
+	movq	%r8, 32(%rsp)
+	xorl	%esi, %esi
+	movl	20(%rsp), %r10d
+	movl	28(%rsp), %r11d
+	movq	%r9, 40(%rsp)
 .L6:
-	leal	3(%r8), %eax
-	movl	%r8d, %ecx
-	movl	$16909056, 20(%rsp)
-	andl	$3, %eax
-	movl	%ecx, %r11d
-	movl	$327936, 24(%rsp)
-	movsbq	20(%rsp,%rax), %rax
-	sarb	$4, %r11b
-	movq	%rsi, 64(%rsp)
-	movl	$117638401, 28(%rsp)
-	movsbl	%r11b, %ebx
-	movl	48(%rsp,%rax,4), %r10d
-	leal	2(%r8), %eax
-	incl	%r8d
-	movq	%rdi, 72(%rsp)
-	movl	%r8d, %edx
-	andl	$3, %eax
-	andl	$3, %edx
-	movsbq	20(%rsp,%rax), %rax
-	movsbq	20(%rsp,%rdx), %rdx
-	movl	48(%rsp,%rax,4), %eax
-	movl	48(%rsp,%rdx,4), %edx
-	cmpb	$2, %r11b
+	movl	%esi, %eax
+	movl	%esi, %ebp
+	movl	%esi, %ecx
+	sarb	$4, %al
+	movsbl	%al, %r14d
+	cmpb	$2, %al
 	je	.L2
-	cmpb	$3, %r11b
+	cmpb	$3, %al
 	je	.L3
-	decb	%r11b
+	decb	%al
 	je	.L4
-	xorl	%edx, %eax
+	movl	%edi, %eax
+	xorl	%r11d, %eax
 	andl	%r10d, %eax
-	jmp	.L16
-.L4:
-	movl	%r10d, %r11d
-	xorl	%eax, %r11d
-	andl	%edx, %r11d
-	jmp	.L15
-.L2:
-	xorl	%r10d, %eax
-.L16:
-	xorl	%edx, %eax
-	movl	%eax, %r11d
+	xorl	%r11d, %eax
 	jmp	.L5
+.L4:
+	movl	%r10d, %eax
+	leal	(%rsi,%rsi,4), %ecx
+	xorl	%edi, %eax
+	incl	%ecx
+	andl	%r11d, %eax
+	xorl	%edi, %eax
+	jmp	.L17
+.L2:
+	movl	%r10d, %eax
+	leal	(%rsi,%rsi,2), %ecx
+	xorl	%edi, %eax
+	addl	$5, %ecx
+	xorl	%r11d, %eax
+	jmp	.L17
 .L3:
-	notl	%edx
-	movl	%edx, %r11d
-	orl	%r10d, %r11d
-.L15:
-	xorl	%eax, %r11d
+	movl	%r11d, %eax
+	imull	$7, %esi, %ecx
+	notl	%eax
+	orl	%r10d, %eax
+	xorl	%edi, %eax
+.L17:
+	andl	$15, %ecx
 .L5:
-	movslq	%ebx, %rbp
-	movl	%ecx, %eax
-	movl	%r8d, 8(%rsp)
-	movsbl	28(%rsp,%rbp), %edx
+	incl	%esi
+	movl	%esi, 8(%rsp)
 	fildl	8(%rsp)
-	andl	$15, %eax
-	imull	%edx, %eax
-	movsbl	24(%rsp,%rbp), %edx
-	addl	%edx, %eax
-	cltd
-	idivl	%r9d
-	movslq	%edx, %rax
-	movl	656(%rsp,%rax,4), %edx
-	movl	%ecx, %eax
-	andl	$3, %eax
-	leal	(%rax,%rbx,4), %ecx
-	movslq	%ecx, %rcx
-	movsbl	64(%rsp,%rcx), %ecx
 #APP
 # 21 "main-src.c" 1
 	fsin
@@ -110,24 +98,36 @@ _start:
 # 0 "" 2
 #NO_APP
 	fabs
-	fmuls	.LC1(%rip)
-	cltq
-	movsbq	20(%rsp,%rax), %rbx
-	addl	48(%rsp,%rbx,4), %edx
-	leal	(%rdx,%r11), %eax
+	fmuls	.LC0(%rip)
+	andl	$3, %ebp
+	movslq	%ecx, %rcx
 	fisttpq	8(%rsp)
 	movq	8(%rsp), %rdx
 	addl	%edx, %eax
+	leal	0(%rbp,%r14,4), %edx
+	addl	(%rbx,%rcx,4), %eax
+	movslq	%edx, %rdx
+	addl	%r13d, %eax
+	movl	%r11d, %r13d
+	movsbl	32(%rsp,%rdx), %ecx
 	roll	%cl, %eax
-	addl	%r10d, %eax
-	movl	%eax, 48(%rsp,%rbx,4)
-	cmpl	$64, %r8d
-	jne	.L6
-	vmovdqa	32(%rsp), %xmm1
-	vpaddd	48(%rsp), %xmm1, %xmm0
+	leal	(%rax,%r10), %ecx
+	cmpl	$64, %esi
+	je	.L19
+	movl	%edi, %r11d
+	movl	%r10d, %edi
+	movl	%ecx, %r10d
+	jmp	.L6
+.L19:
+	addq	$64, %rbx
+	addl	%r11d, 16(%rsp)
+	addl	%ecx, 20(%rsp)
+	addl	%r10d, 24(%rsp)
+	addl	%edi, 28(%rsp)
+	cmpq	%rbx, %r12
+	jne	.L7
 	xorl	%ebx, %ebx
-	vmovaps	%xmm0, 32(%rsp)
-.L10:
+.L11:
 	movl	%ebx, %eax
 	movl	%ebx, %edx
 	shrb	%al
@@ -135,32 +135,28 @@ _start:
 	cmpb	$1, %dl
 	movzbl	%al, %eax
 	sbbl	%ecx, %ecx
-	movzbl	32(%rsp,%rax), %eax
+	movzbl	16(%rsp,%rax), %eax
 	andl	$4, %ecx
 	sarl	%cl, %eax
 	andl	$15, %eax
 	leal	48(%rax), %edx
 	cmpb	$9, %al
-	jle	.L9
+	jle	.L10
 	leal	87(%rax), %edx
-.L9:
-	movb	%dl, 64(%rsp)
-	leaq	64(%rsp), %rsi
-	movl	$1, %edx
+.L10:
+	movb	%dl, 32(%rsp)
+	leaq	32(%rsp), %rsi
+	mov	    $1, %dl
 	incl	%ebx
 	movl	$1, %edi
-	mov	    $1, %al
+	mov     $1, %al
     syscall #call	write
 
 	cmpb	$32, %bl
-	jne	.L10
+	jne	.L11
 	xorl	%edi, %edi
-	mov	    $60, %al
+	mov     $60, %al
     syscall #call	exit
+	.align 4
 .LC0:
-	.long	1134044735
-	.long	1070356677
-	.long	-1873822489
-	.long	-1117647624
-.LC1:
 	.long	1333788672
